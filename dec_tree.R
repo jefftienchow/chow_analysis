8c4fa839f118fbae2b17be1e0a38988404c5fea7
Jeff Chow <Jeff@dhcp-18-189-45-117.dyn.MIT.EDU>
2017-09-05 22:01:20 -0400

    added comments

--- dec_tree.R
+++ dec_tree.R
@@ -2,6 +2,8 @@ library(rpart)
 require("RPostgreSQL")
 library(Amelia)
 library(plyr)
+
+#connect SQL database to R
 drv <- dbDriver("PostgreSQL")
 con <- dbConnect(drv, dbname = "mimic",
                  host = "localhost", port = 5432,
@@ -9,6 +11,7 @@ con <- dbConnect(drv, dbname = "mimic",
 
 data <- dbGetQuery(con,"Select * from mimiciii.log_table")
 
+#fills empty cells with the average from that column
 data$avgperc[is.na(data$avgperc)] <- mean(data$avgperc,na.rm=T)
 data$avgbp[is.na(data$avgbp)] <- mean(data$avgbp,na.rm=T)
 data$avggdl[is.na(data$avggdl)] <- mean(data$avggdl,na.rm=T)
@@ -23,15 +26,19 @@ data$avgkg[is.na(data$avgkg)] <- mean(data$avgkg,na.rm=T)
 train <- data[1:48000,]
 test <- data[48001:49444,]
 
+#trains the regression tree model
+#uses the significant variables obtained from the logistic regression for higher accuracy
 model<-rpart(expire~ avgbp + avgmgdl + avgkul + avggdl + avgcmh2o + avgmeql + avgunits,data=train,method="anova")
 #plot(model, uniform=TRUE, main="Regression Tree for Mileage ")
 #text(model, use.n=TRUE, all=TRUE, cex=.8)
-hello<-model$cptable[which.min(model$cptable[,"xerror"]),"CP"]
+cp_val<-model$cptable[which.min(model$cptable[,"xerror"]),"CP"]
 
-pruned<-prune(model,cp=.01)
-pred <- predict(pruned,newdata=subset(test,select=c(2,4,5,6,8,9,10)))
+#prunes the tree to compensate for overfitting
+pruned<-prune(model,cp=cp_val)
+pred <- predict(pruned,newdata=subset(test,select=c(2,3,4,5,6,7,9,)))
 pred <- ifelse(pred > 0.5,1,0)
 
+#compare with the actual data to see the % error
 print(paste('Accuracy',mean(pred==test[,3])))
 plot(pruned, uniform=TRUE, main="Pruned Regression Tree for Mileage")
 text(pruned, use.n=TRUE, all=TRUE, cex=.8)
